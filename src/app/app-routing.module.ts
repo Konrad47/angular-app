5f9ff7f5506d0b3ffe30774aabfc7416ae6ef0da
Konrad47 <messo123messo234@gmail.com>
2023-07-19 15:38:27 +0200

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CartsComponent } from './pages/carts/carts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'carts', component: CartsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
