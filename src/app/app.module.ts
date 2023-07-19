1269e2710b8ffa69fe96d36f5cee08425c1d497c
Konrad47 <messo123messo234@gmail.com>
2023-07-19 15:26:18 +0200

    create products and carts components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CartsComponent } from './pages/carts/carts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CartsComponent
   ],
   imports: [
     BrowserModule,
