9af474b2360a2f97cb0ee4b4f6993e8b44113296
kplichta <konrad.plichta@sim.com.pl>
2023-07-19 18:41:01 +0200

    sort products by title

--- src/app/components/product-table/product-table.component.ts
+++ src/app/components/product-table/product-table.component.ts
@@ -4,8 +4,12 @@ import { Product } from 'src/app/models/Product';
 @Component({
   selector: 'app-product-table',
   templateUrl: './product-table.component.html',
-  styleUrls: ['./product-table.component.css']
+  styleUrls: ['./product-table.component.css'],
 })
 export class ProductTableComponent {
-  @Input() products!: Product[]
+  @Input() products!: Product[];
+
+  get sortedProducts(): Product[] {
+    return this.products.sort((a, b) => a.title.localeCompare(b.title));
+  }
 }
