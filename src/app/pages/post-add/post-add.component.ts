577441b2e627ca2fdce0e23f4d8290f81e1b6a54
kplichta <konrad.plichta@sim.com.pl>
2023-07-22 10:44:39 +0200

    add post form

--- src/app/pages/post-add/post-add.component.ts
+++ src/app/pages/post-add/post-add.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Post } from 'src/app/models/Post';
 
 @Component({
   selector: 'app-post-add',
   templateUrl: './post-add.component.html',
-  styleUrls: ['./post-add.component.css']
+  styleUrls: ['./post-add.component.css'],
 })
 export class PostAddComponent {
-
+  addPost(post: Post) {
+    console.log(post);
+  }
 }
