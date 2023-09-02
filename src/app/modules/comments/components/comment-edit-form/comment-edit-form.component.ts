049349e89b20904eb29958ece61041433a6e6a7c
kplichta <konrad.plichta@sim.com.pl>
2023-09-02 15:56:13 +0200

    create comment edit functionality

--- src/app/modules/comments/components/comment-edit-form/comment-edit-form.component.ts
+++ src/app/modules/comments/components/comment-edit-form/comment-edit-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Comment } from 'src/app/core/models/comment.model';
 
 @Component({
   selector: 'app-comment-edit-form',
   templateUrl: './comment-edit-form.component.html',
-  styleUrls: ['./comment-edit-form.component.css']
+  styleUrls: ['./comment-edit-form.component.css'],
 })
 export class CommentEditFormComponent {
+  @Output() onEditComment: EventEmitter<Comment> = new EventEmitter();
+  @Input() comment!: Comment;
 
+  constructor() {}
+
+  onSubmit() {
+    const edtComment: Comment = {
+      user: {
+        username: this.comment.user.username,
+      },
+      body: this.comment.body,
+    };
+    if (this.comment.user.username === '' || this.comment.body === '') {
+      alert('Please, fill in a form');
+      return;
+    }
+
+    this.onEditComment.emit(edtComment);
+    alert('Product edited');
+  }
 }
